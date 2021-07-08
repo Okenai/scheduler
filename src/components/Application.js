20298a3dd8d7aebdc671040dea69ac2cb78f6af0
Okenai <kassembayeva@gmail.com>
2021-07-07 20:23:18 -0600

    all components are ready

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,15 +1,94 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
+import DayList from "components/DayList";
+import Appointment from "./Appointment";
+
+const appointmentsData = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Jane Doe",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+  },
+  {
+    id: 5,
+    time: "2pm",
+    interview: {
+      student: "John Doe",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  }
+];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday")
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+  axios
+  .get('/api/days')
+  .then((response)=> {
+   setDays([...response.data])
+  }) 
+  }, [])
+
+  console.log(day)
+  const appointments = appointmentsData.map(appointment => {
+    return (<Appointment key={appointment.id} {...appointment} />)
+  })
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">  <DayList
+          days={days}
+          day={day}
+          setDay={setDay} /></nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
---
+++ src/components/InterviewerList.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import "components/InterviewerList.scss";
+import InterviewerListItem from './InterviewerListItem';
+
+export default function InterviewerList(props) {
+  const MappedInterviewers = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
+        selected={props.interviewer === interviewer.id}
+      />
+    )
+  })
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {MappedInterviewers}
+      </ul>
+    </section>
+  )
+
+}
